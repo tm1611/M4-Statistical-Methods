c6d1d88631dd9412135512aad88591f0110c3b01
tm1611 <tmeiendresch@gmx.de>
2019-12-11 21:45:57 +0100

    function that gives Smyl by subsubset

--- src/extract_top_m4_forecasts.R
+++ src/extract_top_m4_forecasts.R
@@ -5,24 +5,51 @@
 #untar("M4comp2018_0.2.0.tar.gz", list=TRUE)
 #untar("M4comp2018_0.2.0.tar.gz", files="M4comp2018/data/M4.rda")
 
+rm(list=ls())
+
 # read in unpacked file
 #readRDS("M4comp2018/data/M4.rda")
 load(file = "M4comp2018/data/M4.rda")
 load(file = "M4comp2018/data/submission_info.rda")
 
+rm(df)
 df <- Filter(function(df) df$period=="Monthly" & df$type=="Other", M4)
-#df <- Filter(function(df) df$period=="Monthly", M4)
+#df <- Filter(function(df) df$period=="Weekly", M4)
 #df <- give_sam(df, 10)
-rm(M4)
+
+#rm(M4)
 as.character(df[[1]]$period); as.character(df[[1]]$type);length(df)
 
 source("src/my_utils.R")
-df <- give_sam(df, 3)
 
-series <- df[[1]]
-str(series)
+calc_smyl <- function(input){
+  fc_smyl <- input$pt_ff[1,]
+  insample <- input$x
+  outsample <- input$xx
+  
+  sMAPE <- cal_sMAPE(outsample=outsample, forecasts=fc_smyl)
+  MASE <- cal_MASE(insample=insample, outsample = outsample, forecasts = fc_smyl)
 
-forecast <- series$pt_ff
-forecast[1:2,] # first two, i.e. Smyl, and MM
+  output <- list(sMAPE=sMAPE, MASE=MASE)
+  return(output)
+}
 
+fc_names <- c("Smyl")
+Total_sMAPE <- Total_MASE <- matrix(data = NA, nrow = length(df), ncol = length(fc_names))
+colnames(Total_sMAPE) <- colnames(Total_MASE) <- fc_names
+dim(Total_MASE)
+
+
+for (i in 1:length(df)){
+  output <- calc_smyl(df[[i]])
+  Total_sMAPE[i,] <- output$sMAPE
+  Total_MASE[i,] <- output$MASE
+}
+
+sMAPE_mean <- round( (colMeans(Total_sMAPE, na.rm = T)),4 )
+MASE_mean <- round( colMeans(Total_MASE, na.rm=T),4 )
+
+as.character(df[[1]]$period); as.character(df[[1]]$type);length(df)
+sMAPE_mean
+MASE_mean
 
