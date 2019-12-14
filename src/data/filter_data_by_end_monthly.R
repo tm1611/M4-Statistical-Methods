2fe28a69c0a9bfca60c4b592cef1843303e2f4ff
tm1611 <tmeiendresch@gmx.de>
2019-12-14 22:59:15 +0100

    mod end_monthly

--- src/data/filter_data_by_end_monthly.R
+++ src/data/filter_data_by_end_monthly.R
@@ -11,19 +11,15 @@ library(tidyverse)
 library(parallel)
 library(anytime)
 library(purrr)
-library(ggplot2)
-
 library(xts)
 library(zoo)
-
 
 data(M4)
 #df <- Filter(function(df) df$period=="Yearly" & df$type=="Other", M4)
 df <- Filter(function(df) df$period=="Monthly", M4)
 rm(M4)
 
-#df <- give_sam(df, 1)
-#df <- df[1:100]
+# consistency check
 as.character(df[[1]]$period); as.character(df[[1]]$type);length(df)
 
 ### get end of data
@@ -44,72 +40,167 @@ for (i in 1:length(df)){
   end[i] <- time(series)[length(series)]
 }
 
-my_df <- data.frame(item_id=item_id, length=length, end=end)
-dim(my_df)
-head(my_df)
+df_prop <- data.frame(item_id=item_id, length=length, end=end)
+dim(df_prop)
+head(df_prop)
 
 # Function that counts the series by enddate!!!
-my_df %>% 
+df_prop %>% 
+  filter(end < 2020) %>% 
+  filter(end > 2007) %>% 
   count(end) %>% 
+  filter(n > 50) %>% 
   arrange(desc(n)) %>% 
-  head(10) %>% 
-  data.frame()
+  round(2) %>% 
+  data.frame() -> df_end_n
 
+head(df_end_n, 10)
+
+# distribution of end dates
+ggplot(df_end_n, aes(x = end, y=n)) +
+  geom_bar(stat="identity")
+
+
+##################################
+##################################
 # subset 1: N=15700 - 5/2015
-my_df %>% 
+df_prop %>% 
   filter(end > 2015.4) %>% 
   filter(end < 2015.5) %>% 
   count()
 
-# subset 2: N=1224 - 2014
-my_df %>% 
-  filter(end==2014) %>% 
-  count()
+df_prop %>% 
+  filter(end > 2015.4) %>% 
+  filter(end < 2015.5) %>% 
+  select(item_id) -> m4_monthly_052015_ids
 
-# subset 2b: N=1807 - 03/2014
-my_df %>% 
+target_list1 <- as.character(m4_monthly_052015_ids[,])
+
+length(target_list1)
+head(target_list1)
+class(target_list1)
+class(df[[1]]$st)
+
+m4_monthly_end052015 <- Filter(function(df) df$st %in% target_list1, df)
+as.character(m4_monthly_end052015[[1]]$period)
+as.character(m4_monthly_end052015[[1]]$type)
+length(m4_monthly_end052015)
+
+##################################
+##################################
+# subset 2: N=1807 - 03/2014
+df_prop %>% 
   filter(end>2014.2) %>% 
   filter(end<2014.3) %>% 
   count()
 
-# subset 3: 943
-my_df %>% 
+df_prop %>% 
+  filter(end > 2014.2) %>% 
+  filter(end < 2014.3) %>% 
+  select(item_id) -> m4_monthly_032014_ids
+
+target_list2 <- as.character(m4_monthly_032014_ids[,])
+
+length(target_list2)
+head(target_list2)
+class(target_list2)
+class(df[[1]]$st)
+
+m4_monthly_end032014 <- Filter(function(df) df$st %in% target_list2, df)
+as.character(m4_monthly_end032014[[1]]$period)
+as.character(m4_monthly_end032014[[1]]$type)
+length(m4_monthly_end032014)
+
+
+##################################
+##################################
+# subset 3: 943 - 09/2007
+df_prop %>% 
   filter(end > 2007.7) %>% 
   filter(end < 2007.8) %>% 
   count()
   
+df_prop %>% 
+  filter(end > 2007.7) %>% 
+  filter(end < 2007.8) %>% 
+  select(item_id) -> m4_monthly_092007_ids
 
-# filter and get top7
-my_df %>% 
-  count(end_year) %>% 
-  filter(end_year <= 2020) %>% 
-  arrange(desc(n)) 
+target_list3 <- as.character(m4_monthly_092007_ids[,])
 
-# end_date distribution
-my_df %>% 
-  count(end) %>%
-  filter(end <= 2020) %>% 
-  filter(n > 250) %>% 
-  data.frame()
+length(target_list3)
+head(target_list3)
+class(target_list3)
+class(df[[1]]$st)
 
-dim(df_end_n)
-head(df_end_n)
-str(df_end_n)
-summary(df_end_n)
+m4_monthly_end092007 <- Filter(function(df) df$st %in% target_list3, df)
+as.character(m4_monthly_end092007[[1]]$period)
+as.character(m4_monthly_end092007[[1]]$type)
+length(m4_monthly_end092007)
 
-df_end_n$end <- round(df_end_n$end,2)
 
-ggplot(data=df_end_n, aes(x=end, y=n)) +
-  geom_histogram(stat="identity")
+##################################
+##################################
+# Export data to JSON
 
-my_df %>% 
-  count(end) %>%
-  filter(end <= 2020) %>% 
-  ggplot(aes(end)) +
-  geom_histogram()
+length(m4_monthly_end052015) # df1
+length(m4_monthly_end032014) # df2
+length(m4_monthly_end092007) # df3
 
-### To do: Export data to JSONlines
-### To do: Own Benchmarks on the data
+ts_to_json <- function(idx, df, test_data=FALSE, true_dates=FALSE, domain_cat=FALSE){
+  train <- df[[idx]]$x
+  test <- df[[idx]]$xx
+  item_id <- df[[idx]]$st
+  
+  if (domain_cat == FALSE){
+    feat_static_cat <- list(idx)
+  } else {
+    feat_static_cat <- c(idx, as.numeric(df[[idx]]$type))
+  }
+  
+  if (true_dates == FALSE){
+    start <- "1750-01-01 00:00:00"
+  } else {
+    start <- paste0(as.character(as.Date(time(train))[1]), " 00:00:00") 
+  }
+  
+  if (test_data==FALSE){
+    target=train
+  } else {
+    target=c(train, test)
+  }
+  
+  my_list <- list(
+    start=start,
+    item_id=item_id,
+    target=target,
+    feat_static_cat = feat_static_cat
+  )
+  
+  json <- paste0(toJSON(my_list, auto_unbox=TRUE), "\n")
+  #json <- toJSON(my_list, auto_unbox=TRUE, pretty=FALSE)
+  
+  return(json)
+}
+
+###################################
+###################################
+
+# train file
+json1 <- map(1:length(m4_monthly_end092007), ts_to_json, m4_monthly_end092007, test_data=FALSE, true_dates=FALSE, domain_cat=FALSE)
+
+sink("data/json/m4_monthly_end092007_train.json")
+json1 <- lapply(json1, cat)
+sink()
+
+# test file
+json2 <- map(1:length(m4_monthly_end092007), ts_to_json, m4_monthly_end092007, test_data=TRUE, true_dates=FALSE, domain_cat=FALSE)
+
+sink("data/json/m4_monthly_end092007_test.json")
+json2 <- lapply(json2, cat)
+sink()
+
+
+
 
 
 
